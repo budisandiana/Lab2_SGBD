6565fb4a6beb5477e0d1e6a7ebdb85c76f1fdd5b
budisandiana <61983236+budisandiana@users.noreply.github.com>
2021-04-01 23:40:11 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace Lab2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openButton = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.insertButton = new System.Windows.Forms.Button();
+            this.updateButton = new System.Windows.Forms.Button();
+            this.deleteButton = new System.Windows.Forms.Button();
+            this.diagnosticTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.simptomsTextBox = new System.Windows.Forms.TextBox();
+            this.illnessTextBox = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // openButton
+            // 
+            this.openButton.BackColor = System.Drawing.SystemColors.Control;
+            this.openButton.Location = new System.Drawing.Point(822, 273);
+            this.openButton.Name = "openButton";
+            this.openButton.Size = new System.Drawing.Size(221, 34);
+            this.openButton.TabIndex = 0;
+            this.openButton.Text = "Open connection";
+            this.openButton.UseVisualStyleBackColor = false;
+            this.openButton.Click += new System.EventHandler(this.openButton_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.InactiveCaption;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 31);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 29;
+            this.dataGridView1.Size = new System.Drawing.Size(612, 276);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.BackgroundColor = System.Drawing.SystemColors.InactiveCaption;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(12, 381);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowHeadersWidth = 51;
+            this.dataGridView2.Size = new System.Drawing.Size(612, 276);
+            this.dataGridView2.TabIndex = 2;
+            this.dataGridView2.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView2_CellClick);
+            // 
+            // insertButton
+            // 
+            this.insertButton.Location = new System.Drawing.Point(670, 615);
+            this.insertButton.Name = "insertButton";
+            this.insertButton.Size = new System.Drawing.Size(141, 42);
+            this.insertButton.TabIndex = 6;
+            this.insertButton.Text = "insert";
+            this.insertButton.UseVisualStyleBackColor = true;
+            this.insertButton.Click += new System.EventHandler(this.insertButton_Click);
+            // 
+            // updateButton
+            // 
+            this.updateButton.Location = new System.Drawing.Point(848, 615);
+            this.updateButton.Name = "updateButton";
+            this.updateButton.Size = new System.Drawing.Size(141, 42);
+            this.updateButton.TabIndex = 7;
+            this.updateButton.Text = "update";
+            this.updateButton.UseVisualStyleBackColor = true;
+            this.updateButton.Click += new System.EventHandler(this.updateButton_Click);
+            // 
+            // deleteButton
+            // 
+            this.deleteButton.Location = new System.Drawing.Point(1021, 615);
+            this.deleteButton.Name = "deleteButton";
+            this.deleteButton.Size = new System.Drawing.Size(141, 42);
+            this.deleteButton.TabIndex = 8;
+            this.deleteButton.Text = "delete";
+            this.deleteButton.UseVisualStyleBackColor = true;
+            this.deleteButton.Click += new System.EventHandler(this.deleteButton_Click);
+            // 
+            // diagnosticTextBox
+            // 
+            this.diagnosticTextBox.Location = new System.Drawing.Point(899, 452);
+            this.diagnosticTextBox.Name = "diagnosticTextBox";
+            this.diagnosticTextBox.Size = new System.Drawing.Size(232, 27);
+            this.diagnosticTextBox.TabIndex = 9;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(735, 459);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(101, 20);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Diagnostic_ID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(735, 517);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 20);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Simptoms";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(735, 404);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(70, 20);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "illness_ID";
+            // 
+            // simptomsTextBox
+            // 
+            this.simptomsTextBox.Location = new System.Drawing.Point(899, 510);
+            this.simptomsTextBox.Name = "simptomsTextBox";
+            this.simptomsTextBox.Size = new System.Drawing.Size(232, 27);
+            this.simptomsTextBox.TabIndex = 13;
+            // 
+            // illnessTextBox
+            // 
+            this.illnessTextBox.Location = new System.Drawing.Point(899, 397);
+            this.illnessTextBox.Name = "illnessTextBox";
+            this.illnessTextBox.Size = new System.Drawing.Size(232, 27);
+            this.illnessTextBox.TabIndex = 14;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1189, 688);
+            this.Controls.Add(this.illnessTextBox);
+            this.Controls.Add(this.simptomsTextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.diagnosticTextBox);
+            this.Controls.Add(this.deleteButton);
+            this.Controls.Add(this.updateButton);
+            this.Controls.Add(this.insertButton);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.openButton);
+            this.Name = "Form1";
+            this.Text = "CommandBuildForm";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button openButton;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Button insertButton;
+        private System.Windows.Forms.Button updateButton;
+        private System.Windows.Forms.Button deleteButton;
+        private System.Windows.Forms.TextBox diagnosticTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox simptomsTextBox;
+        private System.Windows.Forms.TextBox illnessTextBox;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,265 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Lab2
+{
+    public partial class Form1 : Form
+    {        
+
+        public Form1()
+        {
+            //initializeaza aplicatia (windows forms)
+            InitializeComponent();
+
+            //la inceput setam ca butoanele sa nu poata fi apasate pana nu este apasat butonul open
+            insertButton.Enabled = false;
+            updateButton.Enabled = false;
+            deleteButton.Enabled = false;
+        }
+
+
+        private void openButton_Click(object sender, EventArgs e)
+        {
+            //connectionString => se conecteaza la baza de date Hospital
+            string str = "Data Source= .\\SQLEXPRESS;Initial Catalog=Hospital;Integrated Security=True;MultipleActiveResultSets=True";
+            SqlConnection connection = new SqlConnection(str);
+            SqlDataAdapter adapter; //bridge between DataSet and Sql Server => preia si salveaza datele
+
+            string illnessQuery = "select * from illness";
+            string diagnosticQuery = "select * from Diagnostic";
+            
+            //create a DataSet
+            DataSet datas1 = new DataSet();
+
+            try
+            {                
+                connection.Open(); //deschide conexiunea 
+                adapter = new SqlDataAdapter(illnessQuery, connection);
+                adapter.Fill(datas1, "illness"); //adapter.Fill schimba datele din DataSet ca sa fie egale cu datele din tabelul dat (illness)
+                adapter = new SqlDataAdapter(diagnosticQuery, connection);
+                adapter.Fill(datas1, "Diagnostic"); //adapter.Fill schimba datele din DataSet ca sa fie egale cu datele din tabelul dat (Diagnostic)
+
+                //facem legatura dintre illness (tabelul parinte) si Diagnostic (tabelul copil)
+                datas1.Relations.Add("illness Diagnostic",
+                    datas1.Tables["illness"].Columns["illness_ID"],
+                    datas1.Tables["Diagnostic"].Columns["illness_ID"]);
+                connection.Close(); //inchidem conexiunea
+
+                dataGridView1.DataSource = datas1.Tables["illness"];
+            }
+
+            //daca nu reuseste sa faca conexiunea cu baza de date, atunci va afisa eroare
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+                connection.Close();
+            }
+        }
+
+        //functie ca sa ma ajute sa vad datele in timp real atunci cand fac insert/delete/update
+        private void data_display()
+        {
+            //get the row number on the DataGrid
+            int rownr = this.dataGridView1.CurrentCell.RowIndex;
+
+            //get the content of the cell with the Id
+            object cellvalue = this.dataGridView1[0, rownr].Value; //luam prima coloana din tabel, care e illness_ID
+            string idString = cellvalue.ToString();
+            illnessTextBox.Text = idString; //va aparea id-ul in primul textBox
+            int id = Int32.Parse(idString);
+
+            //create connection
+            string str = "Data Source= .\\SQLEXPRESS;Initial Catalog=Hospital;Integrated Security=True;MultipleActiveResultSets=True";
+            SqlConnection connection = new SqlConnection(str);
+
+            //create the command and parameter objects
+            string childQuery = "select * from Diagnostic where illness_ID = @illness_ID";
+            SqlCommand cmd = new SqlCommand(childQuery, connection); //se conecteaza child table-ul si parent table
+            cmd.Parameters.Add("@illness_ID", SqlDbType.Int);
+            cmd.Parameters["@illness_ID"].Value = id;
+
+            try
+            {
+                connection.Open(); //deschidem conexiunea
+                SqlDataReader reader = cmd.ExecuteReader(); //citeste datele
+                DataTable childTable = new DataTable();
+                childTable.Load(reader);
+                dataGridView2.DataSource = childTable; //afiseaza in DataGridView2 child table-ul
+                reader.Close(); //inchide conexiunea
+            }
+
+            //daca nu reuseste sa faca conexiunea cu baza de date, atunci va afisa eroare
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            data_display(); //apelam functia pentru a face conexiunea la baza de date si a afisa tabelul parinte in DataGridView1
+
+            insertButton.Enabled = true; //pentru ca avem id-ul din tabelul parinte (illness) deja in textbox (si este setat la readOnly), 
+                                         //putem sa inseram in tabelul copil (diagnostic) doar valori cu id-ul selectat
+            updateButton.Enabled = false; //celelalte butoane raman oprite pana apasam pe id-ul din tabelul copil
+            deleteButton.Enabled = false;
+            illnessTextBox.ReadOnly = true;
+        }
+
+        private void dataGridView2_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            //get the row number on the DataGrid
+            int rowNr = this.dataGridView2.CurrentCell.RowIndex;
+
+            //get the content of the cells
+            object cellvalue0 = this.dataGridView2[0, rowNr].Value;
+            object cellvalue1 = this.dataGridView2[1, rowNr].Value;
+            object cellvalue2 = this.dataGridView2[2, rowNr].Value;
+
+            //fiecare textbox este asociat cu cate o coloana din tabel
+            diagnosticTextBox.Text = cellvalue0.ToString();
+            illnessTextBox.Text = cellvalue1.ToString();
+            simptomsTextBox.Text = cellvalue2.ToString();
+
+            //pentru ca toate datele sunt completate in textbox-uri, acum putem sa facem update si delete
+            deleteButton.Enabled = true;
+            updateButton.Enabled = true;
+            insertButton.Enabled = true;
+            illnessTextBox.ReadOnly = false;
+        }
+
+
+        private void deleteButton_Click(object sender, EventArgs e)
+        {
+            //get the row number on the DataGrid
+            int rowNr = this.dataGridView2.CurrentCell.RowIndex;
+
+            //get the content of the cells
+            object cellvalue0 = this.dataGridView2[0, rowNr].Value;
+            string idString = cellvalue0.ToString();
+            diagnosticTextBox.Text = idString;
+            int id = Int32.Parse(idString);
+
+
+            //connectionString
+            string str = "Data Source= .\\SQLEXPRESS;Initial Catalog=Hospital;Integrated Security=True;MultipleActiveResultSets=True";
+            SqlConnection connection = new SqlConnection(str);
+            SqlDataAdapter adapter = new SqlDataAdapter();
+
+            //create the command and parameter objects
+            string deleteQuery = "delete from Diagnostic where Diagnostic_ID= @Diagnostic_ID";
+            adapter.DeleteCommand = new SqlCommand(deleteQuery, connection);
+            adapter.DeleteCommand.Parameters.Add("@Diagnostic_ID", SqlDbType.Int);
+            adapter.DeleteCommand.Parameters["@Diagnostic_ID"].Value = id;
+
+            try
+            {
+                connection.Open();
+                adapter.DeleteCommand.ExecuteNonQuery();
+                data_display(); //apelam functia ca atunci cand face delete, sa afiseze din nou tabelul cu datele noi
+                connection.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+                connection.Close();
+            }
+
+            //resetam datele din textbox-uri
+            illnessTextBox.Text = ""; 
+            diagnosticTextBox.Text = "";
+            simptomsTextBox.Text = "";
+
+        }
+
+        private void updateButton_Click(object sender, EventArgs e)
+        {
+            //get the row number on the DataGrid
+            int rowNr = this.dataGridView2.CurrentCell.RowIndex;
+
+            //get the content of the cells
+            object cellvalue0 = this.dataGridView2[0, rowNr].Value;
+            string idString = cellvalue0.ToString();
+            diagnosticTextBox.Text = idString;
+
+            //connectionString
+            string str = "Data Source= .\\SQLEXPRESS;Initial Catalog=Hospital;Integrated Security=True;MultipleActiveResultSets=True";
+            SqlConnection connection = new SqlConnection(str);
+            SqlDataAdapter adapter = new SqlDataAdapter();
+
+            //create the command and parameter objects
+            string updateQuery = "update Diagnostic set Diagnostic_ID = @Diagnostic_ID, illness_ID = @illness_ID, Simptoms = @Simptoms where Diagnostic_ID = @newID";
+            adapter.UpdateCommand = new SqlCommand(updateQuery, connection);
+            adapter.UpdateCommand.Parameters.Add("@newID", SqlDbType.Int);
+            adapter.UpdateCommand.Parameters["@newID"].Value = cellvalue0.ToString();
+
+            adapter.UpdateCommand.Parameters.Add("@Simptoms", SqlDbType.VarChar, 30);
+            adapter.UpdateCommand.Parameters["@Simptoms"].Value = simptomsTextBox.Text;
+            try
+            {
+                adapter.UpdateCommand.Parameters.Add("@Diagnostic_ID", SqlDbType.Int);
+                adapter.UpdateCommand.Parameters["@Diagnostic_ID"].Value = Int32.Parse(diagnosticTextBox.Text);
+
+                adapter.UpdateCommand.Parameters.Add("@illness_ID", SqlDbType.Int);
+                adapter.UpdateCommand.Parameters["@illness_ID"].Value = Int32.Parse(illnessTextBox.Text);
+
+                connection.Open();
+                adapter.UpdateCommand.ExecuteNonQuery();
+                data_display(); //apelam functia ca atunci cand face update, sa afiseze din nou tabelul cu datele noi
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+                connection.Close();
+            }
+
+            illnessTextBox.Text = "";
+            diagnosticTextBox.Text = "";
+            simptomsTextBox.Text = "";
+        }
+
+        private void insertButton_Click(object sender, EventArgs e)
+        {
+
+            //connectionString
+            string str = "Data Source= .\\SQLEXPRESS;Initial Catalog=Hospital;Integrated Security=True;MultipleActiveResultSets=True";
+            SqlConnection connection = new SqlConnection(str);
+            SqlDataAdapter adapter = new SqlDataAdapter();
+
+
+            //create the command and parameter objects
+            string insertQuery = "insert into Diagnostic values(@Diagnostic_ID, @illness_ID, @Simptoms)";
+            adapter.InsertCommand = new SqlCommand(insertQuery, connection);
+            adapter.InsertCommand.Parameters.Add("@illness_ID", SqlDbType.Int);
+            adapter.InsertCommand.Parameters["@illness_ID"].Value = Int32.Parse(illnessTextBox.Text);
+            adapter.InsertCommand.Parameters.Add("@Simptoms", SqlDbType.VarChar, 30);
+            adapter.InsertCommand.Parameters["@Simptoms"].Value = simptomsTextBox.Text;
+
+            try
+            {
+                adapter.InsertCommand.Parameters.Add("@Diagnostic_ID", SqlDbType.Int);
+                adapter.InsertCommand.Parameters["@Diagnostic_ID"].Value = Int32.Parse(diagnosticTextBox.Text);
+
+                connection.Open();
+                adapter.InsertCommand.ExecuteNonQuery();
+                data_display(); //apelam functia ca atunci cand face insert, sa afiseze din nou tabelul cu datele noi
+                connection.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+                connection.Close();
+            }
+
+            diagnosticTextBox.Text = "";
+            simptomsTextBox.Text = "";
+        }
+
+    }
+}
